5cef5af18623d9efedbf1d051d7b7c4c26607905
Sergey Davkov <sergey.davkov@gmail.com>
2022-01-06 14:56:09 +0400

    Modal: Esc button

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -4,10 +4,23 @@ import PropTypes from 'prop-types'
 import ModalOverlay from '../modal-overlay/modal-overlay.js';
 import styles from './modal.module.css'
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useEffect } from 'react';
 
 const modalRoot = document.getElementById("react-modals");
 
 const Modal = React.memo(({ visible, setVisible, title, children }) => {
+
+    const keyDown = (e) => {
+        if (e.keyCode === 27)
+            setVisible(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', keyDown)
+        return () => {
+            window.removeEventListener('keydown', keyDown)
+        }
+    })
 
     return ReactDOM.createPortal(
         visible && (
