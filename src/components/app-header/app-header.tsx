3e260e48f30d7ae2a6612f5a1e71ce5868943e35
Sergey Davkov <sergey.davkov@gmail.com>
2022-02-25 17:05:05 +0400

    TS: Header

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,30 @@
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import React from 'react'
+import AppHeaderItem from '../app-header-item/app-header-item'
+import styles from './app-header.module.css'
+
+const AppHeader = React.memo(() => {
+    return (
+        <header className={styles.header}>
+            <div className="container">
+                <div className={styles.row + ' pt-4 pb-4'}>
+                    <nav className={styles.nav}>
+                        <AppHeaderItem text='Конструктор' extCssClasses='pt-5 pr-5 pb-5' to='/'><BurgerIcon
+                            type="secondary" /></AppHeaderItem>
+                        <AppHeaderItem text='Лента заказов' extCssClasses="p-5"><ListIcon
+                            type="secondary" /></AppHeaderItem>
+                    </nav>
+                    <div className={styles.logo}>
+                        <Logo />
+                    </div>
+                    <div className={styles.profile}>
+                        <AppHeaderItem text='Личный кабинет' extCssClasses='pt-5 pl-5 pb-5' to='/profile'><ProfileIcon
+                            type="secondary" /></AppHeaderItem>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+})
+
+export default AppHeader
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import ModalOverlay from '../modal-overlay/modal-overlay.js';
+import ModalOverlay from '../modal-overlay/modal-overlay';
 import styles from './modal.module.css'
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useEffect } from 'react';
